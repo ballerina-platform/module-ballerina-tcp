afd3822774a5539ececb525f779f3078ba2b2056
MohamedSabthar <sabtharugc0@gmail.com>
2021-01-29 13:07:57 +0530

    Add exceptionCaught() to TestUtils.serverHandler

--- tcp-test-utils/src/main/java/org/ballerinalang/stdlib/tcp/testutils/ServerHandler.java
+++ tcp-test-utils/src/main/java/org/ballerinalang/stdlib/tcp/testutils/ServerHandler.java
@@ -3,18 +3,22 @@ package org.ballerinalang.stdlib.tcp.testutils;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.CharsetUtil;
 
+@ChannelHandler.Sharable
 public class ServerHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf buf) throws Exception {
-        System.out.println("Received by test server :" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("Received by test server : " + buf.toString(CharsetUtil.UTF_8));
         if (!buf.toString(CharsetUtil.UTF_8).equals("Do not reply")) {
             ctx.channel().writeAndFlush(buf);
         }
-        ctx.close();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("Test server : " + cause.getMessage());
     }
 }
